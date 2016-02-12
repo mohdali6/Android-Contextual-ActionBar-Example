307a1f54cc8acb2fbfa586b0deeeb69d64a653f9
Mohd Ali <mohd.ali6@yahoo.com>
2016-02-12 12:36:13 +0530

    Add full functionality of CAB delete menu item

--- app/src/main/java/com/example/mali/contextualactionbarexample/ListAdapter.java
+++ app/src/main/java/com/example/mali/contextualactionbarexample/ListAdapter.java
@@ -5,9 +5,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
+import java.util.ArrayList;
+
 public class ListAdapter extends ArrayAdapter<String> {
 
-    public ListAdapter(Context context, int resource, int textViewResourceId, String[] objects) {
+    public ListAdapter(Context context, int resource, int textViewResourceId, ArrayList<String> objects) {
         super(context, resource, textViewResourceId, objects);
     }
 
--- app/src/main/java/com/example/mali/contextualactionbarexample/MainActivity.java
+++ app/src/main/java/com/example/mali/contextualactionbarexample/MainActivity.java
@@ -9,21 +9,22 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.AbsListView;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String[] data = {
-            "One",
-            "Two",
-            "Three",
-            "Four",
-            "Five",
-            "Six",
-            "Seven",
-            "Eight",
-            "Nine",
-            "Ten"
-    };
+    private final String SELECTION_COUNT = "selection_count";
+    private final String SELECTED_ITEMS_HASHSET = "selected_items_hashset";
+    private final String DATA_ARRAY_LIST = "data_array_list";
+    private ListAdapter mAdapter;
+    private ArrayList<String> data;
+    private int selectionCount;
+    private TreeSet<Integer> selectedItemsPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +33,35 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ListAdapter mAdapter = new ListAdapter(this,
+        if (savedInstanceState != null) {
+            selectedItemsPosition = (TreeSet<Integer>) savedInstanceState
+                    .getSerializable(SELECTED_ITEMS_HASHSET);
+            selectionCount = savedInstanceState.getInt(SELECTION_COUNT);
+            data = savedInstanceState.getStringArrayList(DATA_ARRAY_LIST);
+        } else {
+            selectionCount = 0;
+            selectedItemsPosition = new TreeSet<>();
+            data = new ArrayList<>(
+                    Arrays.asList(
+                            "One",
+                            "Two",
+                            "Three",
+                            "Four",
+                            "Five",
+                            "Six",
+                            "Seven",
+                            "Eight",
+                            "Nine",
+                            "Ten"
+                    )
+            );
+        }
+
+        mAdapter = new ListAdapter(this,
                 R.layout.list_item,
                 R.id.textView,
                 data);
+
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(mAdapter);
 
@@ -44,13 +70,28 @@ public class MainActivity extends AppCompatActivity {
         listView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
             @Override
             public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+                if (checked) {
+                    selectionCount++;
+                    selectedItemsPosition.add(position);
+                } else {
+                    selectionCount--;
+                    selectedItemsPosition.remove(position);
+                }
 
+                if (selectionCount != 0) {
+                    mode.setTitle(Integer.toString(selectionCount));
+                }
             }
 
             @Override
             public boolean onCreateActionMode(ActionMode mode, Menu menu) {
                 MenuInflater inflater = getMenuInflater();
                 inflater.inflate(R.menu.contextual_action_bar_menu, menu);
+
+                if (selectionCount != 0) {
+                    mode.setTitle(Integer.toString(selectionCount));
+                }
+
                 return true;
             }
 
@@ -61,12 +102,26 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-                return false;
+                switch (item.getItemId()) {
+                    case R.id.item_delete:
+                        deleteSelectedItems();
+
+                        String text = Integer.toString(selectionCount).concat(" items deleted!");
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast toast = Toast.makeText(getApplicationContext(), text, duration);
+                        toast.show();
+
+                        mode.finish();
+                        return true;
+                    default:
+                        return false;
+                }
             }
 
             @Override
             public void onDestroyActionMode(ActionMode mode) {
-
+                selectionCount = 0;
+                selectedItemsPosition.clear();
             }
         });
     }
@@ -91,5 +146,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putInt(SELECTION_COUNT, selectionCount);
+        outState.putSerializable(SELECTED_ITEMS_HASHSET, selectedItemsPosition);
+        outState.putStringArrayList(DATA_ARRAY_LIST, data);
+        super.onSaveInstanceState(outState);
+    }
+
+    private void deleteSelectedItems() {
+        Iterator iterator = selectedItemsPosition.iterator();
+
+        int i = 0;
+        while (iterator.hasNext()) {
+            int value = (Integer) iterator.next();
+            data.remove(value - i);
+            i++;
+        }
+        mAdapter.notifyDataSetChanged();
     }
 }
